cece2db6c1132246ad08221e290e4c4cf17b0eee
aimeedejonge <jongeaimee@gmail.com>
2026-03-12 12:13:22 +0100

    Modified main, add code to sleep, sleep_acticit and calories

--- sleep.py
+++ sleep.py
@@ -2,11 +2,10 @@ import pandas as pd
 import sqlite3
 import os
 
-def get_users_sleep_minutes(user_id):
-    con = sqlite3.connect("fitbit_database.db")
-    query = "SELECT Id, TRIM(SUBSTR(date, 1, INSTR(date, ' '))) as clean_date, COUNT(*) as duration_minutes FROM minute_sleep WHERE value >= 1 and Id = ? GROUP BY clean_date"
+def get_users_sleep_minutes(user_id, con):
+    query = "SELECT Id, TRIM(SUBSTR(date, 1, INSTR(date, ' '))) as clean_date, COUNT(*) as duration_minutes, FROM minute_sleep WHERE value >= 1 and Id = ? GROUP BY clean_date"
     df_user_sleep = pd.read_sql_query(query, con, params=(user_id,))
-    
+
     if not df_user_sleep.empty:
         df_user_sleep['Id'] = pd.to_numeric(df_user_sleep['Id'], errors='coerce')
         df_user_sleep['Id'] = df_user_sleep['Id'].astype('Int64').astype(str)
@@ -15,9 +14,7 @@ def get_users_sleep_minutes(user_id):
     con.close()
     return df_user_sleep
 
-def get_global_sleep_minutes():
-
-    con = sqlite3.connect("fitbit_database.db")
+def get_global_sleep_minutes(con):
 
     query = "SELECT Id, COUNT(*) as sleep_minutes, COUNT(DISTINCT substr(date, 1, 10)) as sleep_day_count FROM minute_sleep WHERE value >= 1 GROUP BY Id"
     df_total_sleep = pd.read_sql_query(query, con)
@@ -28,3 +25,17 @@ def get_global_sleep_minutes():
     
     con.close()
     return df_total_sleep
+
+def get_average_sleep(Id, view_by, con):
+    data_df = get_users_sleep_minutes(Id, con)
+
+    if view_by == "Day":
+        data_df["clean_date"] = pd.to_datetime(data_df["clean_date"])
+        xlabel = "Amount of sleep minutes"
+        title = f"Average sleep per day {Id}"
+    else:
+        data_df["clean_date"] = pd.to_datetime(data_df["clean_date"]).by.week
+        xlabel = "Date"
+        title = f"Mean heart rate per day for user {Id}"
+
+    

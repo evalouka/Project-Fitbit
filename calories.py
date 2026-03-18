8e0b5df52e01b4a978883ae784c4e34de4a164cc
aimeedejonge <jongeaimee@gmail.com>
2026-03-18 10:26:39 +0100

    change from mathplot to plotly, include view_by tabs

--- calories.py
+++ calories.py
@@ -1,8 +1,7 @@
 import sqlite3
 import pandas as pd
 from datetime import datetime
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 def plot_user_vs_global_calories(Id, con):
 
@@ -12,7 +11,7 @@ def plot_user_vs_global_calories(Id, con):
     global_query = "SELECT ActivityDate, AVG(Calories) as average_calories FROM daily_activity GROUP BY ActivityDate"
     global_df = pd.read_sql_query(global_query, con)
 
-    con.close()
+    #con.close()
 
     if user_df.empty:
         print(f"No data found for ID: {Id}")
@@ -24,14 +23,31 @@ def plot_user_vs_global_calories(Id, con):
     global_df['ActivityDate'] = pd.to_datetime(global_df['ActivityDate'])
     global_df = global_df.sort_values('ActivityDate')
 
-    plt.figure(figsize=(12, 6))
-    plt.bar(user_df['ActivityDate'], user_df['Calories'], color="#2c94a0", label='User calories burend')
-    plt.plot(global_df['ActivityDate'],global_df['average_calories'], color="#322ca0", label = 'Global calories burned', linestyle = '-')
-    plt.xlim(user_df['ActivityDate'].min(), user_df['ActivityDate'].max())
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+    start_date = user_df['ActivityDate'].min()
+    end_date = user_df['ActivityDate'].max()
 
-    plt.title(f"Calories burned per day")
-    plt.xlabel("Date")
-    plt.ylabel("Calories burned")
-    plt.xticks(rotation=45)
-    plt.show()
+    fig = px.bar(user_df, x='ActivityDate', y='Calories', 
+                 title=f'Calories burned by user {Id} per day'
+                 )
+    fig.update_traces(marker_color="#2c7da0", name="User Calories", showlegend=True)
+
+    line = px.line(global_df, x='ActivityDate', y='average_calories')
+    line_trace = line.data[0]
+    line_trace.update(name='Global Average', 
+                  line=dict(color="#3C27C6", width=4, dash='dash'))
+    
+    fig.add_trace(line_trace)
+
+    fig.update_xaxes(range=[start_date, end_date])
+    fig.update_layout(height = 400,
+                      paper_bgcolor="rgba(0,0,0,0)",
+                      plot_bgcolor="rgba(0,0,0,0)",
+                      xaxis_title="Activity Dates",
+                      yaxis_title="Calories burned",
+                      font_color="white")
+
+    fig.show()
+
+#Id = 4319703577
+#con = sqlite3.connect(r"C:\Users\jonge\PycharmProjects\Data Engineering\Project-Fitbit\fitbit_database.db")
+#plot_user_vs_global_calories(Id, con)
